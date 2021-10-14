45fd7605eaad96ce6df5b427d97e7324ea66031e
root <root@DESKTOP-A43NG01.localdomain>
2021-10-14 23:08:28 +0800

    remove element assets

--- src/components/views/auth/VectorAuthHeaderLogo.tsx
+++ src/components/views/auth/VectorAuthHeaderLogo.tsx
@@ -23,14 +23,15 @@ export default class VectorAuthHeaderLogo extends React.PureComponent {
 
     render() {
         const brandingConfig = SdkConfig.get().branding;
-        let logoUrl = "themes/element/img/logos/element-logo.svg";
-        if (brandingConfig && brandingConfig.authHeaderLogoUrl) {
-            logoUrl = brandingConfig.authHeaderLogoUrl;
-        }
+        // let logoUrl = "themes/element/img/logos/element-logo.svg";
+        const reachLogo = 'themes/element/img/logos/reachLogo.png'
+        // if (brandingConfig && brandingConfig.authHeaderLogoUrl) {
+        //     logoUrl = brandingConfig.authHeaderLogoUrl;
+        // }
 
         return (
             <div className="mx_AuthHeaderLogo">
-                <img src={logoUrl} alt="Element" />
+                <img src={reachLogo} alt="Reach by FoneAPI" />
             </div>
         );
     }
